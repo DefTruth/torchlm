984baf32a481e015e47fccac512e7a5fee5a6162
DefTruth <qiustudent_r@163.com>
2022-02-13 20:52:45 +0800

    feat(pypi): update torchml pypi v0.1.1 (#8)

--- torchlm/__init__.py
+++ torchlm/__init__.py
@@ -1,5 +1,5 @@
 # Versions
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 # Transforms Module: 100+ transforms available, can bind torchvision and
 # albumentations into torchlm pipeline with autodtype wrapper.
 from .transfroms import *
