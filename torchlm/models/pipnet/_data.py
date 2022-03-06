f5b65fbd673315eaab49d153c60611b2ac64ee4c
DefTruth <qiustudent_r@163.com>
2022-03-06 12:43:55 +0800

    feat(PIPNet): add PIPNet to torchlm (#26)

--- torchlm/models/pipnet/_data.py
+++ torchlm/models/pipnet/_data.py
@@ -57,44 +57,45 @@ class _PIPTrainDataset(Dataset):
 
     def __getitem__(self, index: int) -> _PIPTrainDataset_Output_Type:
         annotation_string = self.annotations_info[index]
-        img_path, target = annotools.decode_annotation(annotation_string=annotation_string)
+        img_path, label = annotools.decode_annotation(annotation_string=annotation_string)
         img = cv2.imread(img_path)[:, :, ::-1]  # BGR -> RGB
         if not self.coordinates_already_normalized:
             h, w, _ = img.shape
-            target[:, 0] /= w
-            target[:, 1] /= h
+            label[:, 0] /= w
+            label[:, 1] /= h
 
-        img, target = self.transform(img, target)
+        # online data augmentations
+        img, label = self.transform(img, label)
         # converted to Tensor manually
-        if isinstance(target, Tensor):
-            target = target.cpu().numpy()
+        if isinstance(label, Tensor):
+            label = label.cpu().numpy()
 
         grid_size = int(self.input_size / self.net_stride)
-        target_cls = np.zeros((self.num_lms, grid_size, grid_size))
-        target_x = np.zeros((self.num_lms, grid_size, grid_size))
-        target_y = np.zeros((self.num_lms, grid_size, grid_size))
-        target_nb_x = np.zeros((self.num_nb * self.num_lms, grid_size, grid_size))
-        target_nb_y = np.zeros((self.num_nb * self.num_lms, grid_size, grid_size))
-        target_cls, target_x, target_y, target_nb_x, target_nb_y = _generate_targets(
-            normalized_target=target,
+        label_cls = np.zeros((self.num_lms, grid_size, grid_size))
+        label_x = np.zeros((self.num_lms, grid_size, grid_size))
+        label_y = np.zeros((self.num_lms, grid_size, grid_size))
+        label_nb_x = np.zeros((self.num_nb * self.num_lms, grid_size, grid_size))
+        label_nb_y = np.zeros((self.num_nb * self.num_lms, grid_size, grid_size))
+        label_cls, label_x, label_y, label_nb_x, label_nb_y = _generate_labels(
+            normalized_label=label,
             meanface_indices=self.meanface_indices,
-            target_cls=target_cls,
-            target_x=target_x,
-            target_y=target_y,
-            target_nb_x=target_nb_x,
-            target_nb_y=target_nb_y
+            label_cls=label_cls,
+            label_x=label_x,
+            label_y=label_y,
+            label_nb_x=label_nb_x,
+            label_nb_y=label_nb_y
         )
 
         if not isinstance(img, Tensor):
-            img, target = self.to_tensor(img, target)  # HWC -> CHW
+            img, label = self.to_tensor(img, label)  # HWC -> CHW
 
-        target_cls = torch.from_numpy(target_cls)
-        target_x = torch.from_numpy(target_x)
-        target_y = torch.from_numpy(target_y)
-        target_nb_x = torch.from_numpy(target_nb_x)
-        target_nb_y = torch.from_numpy(target_nb_y)
+        label_cls = torch.from_numpy(label_cls)
+        label_x = torch.from_numpy(label_x)
+        label_y = torch.from_numpy(label_y)
+        label_nb_x = torch.from_numpy(label_nb_x)
+        label_nb_y = torch.from_numpy(label_nb_y)
 
-        return img, target_cls, target_x, target_y, target_nb_x, target_nb_y
+        return img, label_cls, label_x, label_y, label_nb_x, label_nb_y
 
     def __len__(self) -> int:
         return len(self.annotations_info)
@@ -115,61 +116,61 @@ class _PIPEvalDataset(object):
 
     def __getitem__(self, index: int) -> Tuple[np.ndarray, np.ndarray]:  # img, lms_gt
         annotation_string = self.annotations_info[index]
-        img_path, target = annotools.decode_annotation(annotation_string=annotation_string)
+        img_path, label = annotools.decode_annotation(annotation_string=annotation_string)
         img = cv2.imread(img_path)[:, :, ::-1]  # BGR -> RGB
         if self.coordinates_already_normalized:
             h, w, _ = img.shape
-            target[:, 0] *= w
-            target[:, 1] *= h
-        return img, target
+            label[:, 0] *= w
+            label[:, 1] *= h
+        return img, label
 
     def __len__(self) -> int:
         return len(self.annotations_info)
 
 
 # Reference: https://github.com/jhb86253817/PIPNet/blob/master/lib/data_utils.py#L86
-def _generate_targets(
-        normalized_target: np.ndarray,
+def _generate_labels(
+        normalized_label: np.ndarray,
         meanface_indices: List[List[int]],
-        target_cls: np.ndarray,
-        target_x: np.ndarray,
-        target_y: np.ndarray,
-        target_nb_x: np.ndarray,
-        target_nb_y: np.ndarray
+        label_cls: np.ndarray,
+        label_x: np.ndarray,
+        label_y: np.ndarray,
+        label_nb_x: np.ndarray,
+        label_nb_y: np.ndarray
 ):
     """
-    :param normalized_target: np.ndarray normalized gt landmarks with shape (N,2)
+    :param normalized_label: np.ndarray normalized gt landmarks with shape (N,2)
     :param meanface_indices: nearest landmarks indexes for each gt landmarks, List of lists.
-    :param target_cls: np.ndarray e.g (N, 8, 8)
-    :param target_x: np.ndarray e.g (N, 8, 8)
-    :param target_y: np.ndarray e.g (N, 8, 8)
-    :param target_nb_x: np.ndarray e.g (N*10, 8, 8)
-    :param target_nb_y: np.ndarray e.g (N*10, 8, 8)
+    :param label_cls: np.ndarray e.g (N, 8, 8)
+    :param label_x: np.ndarray e.g (N, 8, 8)
+    :param label_y: np.ndarray e.g (N, 8, 8)
+    :param label_nb_x: np.ndarray e.g (N*10, 8, 8)
+    :param label_nb_y: np.ndarray e.g (N*10, 8, 8)
     :return:
     """
     num_nb = len(meanface_indices[0])
-    cls_channel, cls_height, cls_width = target_cls.shape  # (n,8,8)
-    normalized_target = normalized_target.reshape(-1, 2)
-    assert cls_channel == normalized_target.shape[0]
+    cls_channel, cls_height, cls_width = label_cls.shape  # (n,8,8)
+    normalized_label = normalized_label.reshape(-1, 2)
+    assert cls_channel == normalized_label.shape[0]
 
     for i in range(cls_channel):
         # find left-top corner of a anchor in 8x8 grid
-        mu_x = int(floor(normalized_target[i][0] * cls_width))
-        mu_y = int(floor(normalized_target[i][1] * cls_height))
+        mu_x = int(floor(normalized_label[i][0] * cls_width))
+        mu_y = int(floor(normalized_label[i][1] * cls_height))
         mu_x = max(0, mu_x)
         mu_y = max(0, mu_y)
         mu_x = min(mu_x, cls_width - 1)
         mu_y = min(mu_y, cls_height - 1)
-        target_cls[i, mu_y, mu_x] = 1
-        shift_x = normalized_target[i][0] * cls_width - mu_x
-        shift_y = normalized_target[i][1] * cls_height - mu_y
-        target_x[i, mu_y, mu_x] = shift_x
-        target_y[i, mu_y, mu_x] = shift_y
+        label_cls[i, mu_y, mu_x] = 1
+        shift_x = normalized_label[i][0] * cls_width - mu_x
+        shift_y = normalized_label[i][1] * cls_height - mu_y
+        label_x[i, mu_y, mu_x] = shift_x
+        label_y[i, mu_y, mu_x] = shift_y
 
         for j in range(num_nb):
-            nb_x = normalized_target[meanface_indices[i][j]][0] * cls_width - mu_x
-            nb_y = normalized_target[meanface_indices[i][j]][1] * cls_height - mu_y
-            target_nb_x[num_nb * i + j, mu_y, mu_x] = nb_x
-            target_nb_y[num_nb * i + j, mu_y, mu_x] = nb_y
+            nb_x = normalized_label[meanface_indices[i][j]][0] * cls_width - mu_x
+            nb_y = normalized_label[meanface_indices[i][j]][1] * cls_height - mu_y
+            label_nb_x[num_nb * i + j, mu_y, mu_x] = nb_x
+            label_nb_y[num_nb * i + j, mu_y, mu_x] = nb_y
 
-    return target_cls, target_x, target_y, target_nb_x, target_nb_y
+    return label_cls, label_x, label_y, label_nb_x, label_nb_y
