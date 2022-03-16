d44cc7c9fdea99e6e890d088ea6409bde7706160
DefTruth <qiustudent_r@163.com>
2022-03-16 23:26:16 +0800

    feat(onnxruntime): add onnxruntime support for PIPNet (#46)

--- torchlm/runtime/ort/_faceboxesv2.py
+++ torchlm/runtime/ort/_faceboxesv2.py
@@ -1,12 +1,115 @@
 import os
 import cv2
+import torch
 import warnings
 import numpy as np
+from math import ceil
+from torch import Tensor
+from pathlib import Path
 import onnxruntime as ort
+from itertools import product as product
+from typing import Optional, List, Any, Tuple
 
 from .._runtime import FaceDetBase
 
-from typing import Optional, List, Any
+
+# TODO: 改写为numpy版本
+class PriorBox(object):
+    def __init__(self, cfg: dict, image_size: Tuple[int, int] = None):
+        super(PriorBox, self).__init__()
+        self.min_sizes = cfg['min_sizes']
+        self.steps = cfg['steps']
+        self.clip = cfg['clip']
+        assert image_size is not None
+        self.image_size = image_size
+        self.feature_maps = [[ceil(self.image_size[0] / step),
+                              ceil(self.image_size[1] / step)]
+                             for step in self.steps]
+
+    def forward(self) -> Tensor:
+        anchors = []
+        for k, f in enumerate(self.feature_maps):
+            min_sizes = self.min_sizes[k]
+            for i, j in product(range(f[0]), range(f[1])):
+                for min_size in min_sizes:
+                    s_kx = min_size / self.image_size[1]
+                    s_ky = min_size / self.image_size[0]
+                    if min_size == 32:
+                        dense_cx = [x * self.steps[k] / self.image_size[1] for x in
+                                    [j + 0, j + 0.25, j + 0.5, j + 0.75]]
+                        dense_cy = [y * self.steps[k] / self.image_size[0] for y in
+                                    [i + 0, i + 0.25, i + 0.5, i + 0.75]]
+                        for cy, cx in product(dense_cy, dense_cx):
+                            anchors += [cx, cy, s_kx, s_ky]
+                    elif min_size == 64:
+                        dense_cx = [x * self.steps[k] / self.image_size[1] for x in [j + 0, j + 0.5]]
+                        dense_cy = [y * self.steps[k] / self.image_size[0] for y in [i + 0, i + 0.5]]
+                        for cy, cx in product(dense_cy, dense_cx):
+                            anchors += [cx, cy, s_kx, s_ky]
+                    else:
+                        cx = (j + 0.5) * self.steps[k] / self.image_size[1]
+                        cy = (i + 0.5) * self.steps[k] / self.image_size[0]
+                        anchors += [cx, cy, s_kx, s_ky]
+        # back to torch land
+        output = torch.Tensor(anchors).view(-1, 4)
+        if self.clip:
+            output.clamp_(max=1, min=0)
+        return output
+
+
+# cpu NMS
+def _nms(dets: np.ndarray, thresh: float) -> List[int]:
+    """Pure Python NMS baseline."""
+    x1 = dets[:, 0]
+    y1 = dets[:, 1]
+    x2 = dets[:, 2]
+    y2 = dets[:, 3]
+    scores = dets[:, 4]
+
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    order = scores.argsort()[::-1]
+
+    keep = []
+    while order.size > 0:
+        i = order[0]
+        keep.append(i)
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.maximum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
+
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+
+        inds = np.where(ovr <= thresh)[0]
+        order = order[inds + 1]
+
+    return keep
+
+
+# Adapted from https://github.com/Hakuyume/chainer-ssd
+# TODO: 改写为numpy版本
+def _decode(loc: np.ndarray, priors: np.ndarray, variances: List[float]) -> Tensor:
+    """Decode locations from predictions using priors to undo
+    the encoding we did for offset regression at train time.
+    Args:
+        loc (tensor): location predictions for loc layers,
+            Shape: [num_priors,4]
+        priors (tensor): Prior boxes in center-offset form.
+            Shape: [num_priors,4].
+        variances: (list[float]) Variances of priorboxes
+    Return:
+        decoded bounding box predictions
+    """
+
+    boxes = torch.cat((
+        priors[:, :2] + loc[:, :2] * variances[0] * priors[:, 2:],
+        priors[:, 2:] * torch.exp(loc[:, 2:] * variances[1])), 1)
+    boxes[:, :2] -= boxes[:, 2:] / 2
+    boxes[:, 2:] += boxes[:, :2]
+    return boxes
 
 
 class FaceBoxesV2ORT(FaceDetBase):
